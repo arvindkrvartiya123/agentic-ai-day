ffce67eea36e62e830cd128e005c61b5f3a97961
arvindkrvartiya123 <111346724+arvindkrvartiya123@users.noreply.github.com>
2025-07-27 10:21:43 +0530

    Delete agribotics_agent directory

--- agribotics_agent/prompt.py
+++
@@ -1,2 +0,0 @@
-ROOT_AGENT_INSTRUCTION="""
-You are a helpful assistant that can connect users on a video call."""
--- agribotics_agent/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .live_agent import connect_video_call
-
-__all__ = ["connect_video_call"]
